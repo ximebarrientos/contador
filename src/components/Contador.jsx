416275a483f068f1d951a8131610a184c0bdb3a2
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-21 19:24:27 -0300

    agregamos una fn que sume una variable

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,14 @@
 const Contador = () => {
+    let numero=1;
+
+    const sumar=()=>{
+        numero++
+    }
     return (
         <div className="mt-3 text-center">
             <h2 className="tituloContador">Contador</h2>
-            <h3>N°: 1</h3>
-            <button className="btn btn-primary me-2">+</button>
+            <h3>N°: {numero}</h3>
+            <button className="btn btn-primary me-2" onClick={sumar}>+</button>
             <button className="btn btn-danger">-</button>
         </div>
     );
