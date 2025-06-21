da5e118df4b20dfb45f649c58718b4b77d0a525b
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-21 18:59:50 -0300

    agregamos un ejemplo de uso de props en un nuevo componente comision

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Informacion from "./components/Informacion"
+import Comision from "./components/Comision"
 
 function App() {
-  
+  //aqui agregamos la logica del componente
+  const nombreComision="c12"
   return (
+    //aqui puedo usar un poco de logica
     <>
-    <h1>Primer proyecto de react</h1>
-    <Informacion></Informacion>
+    <main className="container my-5">
+      {/* aqui aprego todo el maquetado */}
+      <h1>Primer proyecto de react</h1>
+      <Informacion></Informacion>
+      <Comision nombreComisionProps={nombreComision}></Comision>
+    </main>
     </>
   )
   }
---
+++ src/components/Comision.jsx
@@ -0,0 +1,12 @@
+const Comision = (props) => {
+  return (
+    <div className="bg-info-subtle p-3 rounded-3">
+      <p>
+        Comision <b>{props.nombreComisionProps}</b> 2025. Proyecto contador con
+        react
+      </p>
+    </div>
+  );
+};
+
+export default Comision;
