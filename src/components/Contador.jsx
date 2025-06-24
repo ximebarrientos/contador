f15e6e3c6c06e5a09c26ca55590cfd5b47c71ba7
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-24 19:20:49 -0300

    cambiamos la fn sumar por una fn anonima d euna sola linea

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -3,17 +3,16 @@ import { useState } from "react";
 
 const Contador = () => {
     const [numero,setNumero]=useState(1) //asi declaro un estado
-    //let numero=1;
 
-    const sumar=()=>{
+    /* const sumar=()=>{
         setNumero(numero+1)
         
-    }
+    } */
     return (
         <div className="mt-3 text-center">
             <h2 className="tituloContador">Contador</h2>
             <h3>N°: {numero}</h3>
-            <button className="btn btn-primary me-2" onClick={sumar}>+</button>
+            <button className="btn btn-primary me-2" onClick={()=>setNumero(numero+1)}>+</button>
             <button className="btn btn-danger">-</button>
         </div>
     );
